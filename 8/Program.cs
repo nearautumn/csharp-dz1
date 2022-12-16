a7c79c9cc8acc7d0d6f4a4c01328fccf5785218a
Andrei Aliaksandrau <aleksandrov.a.95@yandex.ru>
2022-12-16 19:18:34 +0300

    example 8 added

---
+++ 8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Все четные числа от 0 до заданного."); 
+Console.Write("Введите целое число: "); 
+int number = int.Parse(Console.ReadLine() ?? "");
+int count = 0;
+
+Console.Write($"{number} -> ");
+
+while(count <= number)
+{
+    Console.Write($"{count}, ");
+    count = count + 2;
+} 
