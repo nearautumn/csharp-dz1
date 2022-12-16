25fdb90619e31fcc4763587e58726131bb0945eb
Andrei Aliaksandrau <aleksandrov.a.95@yandex.ru>
2022-12-16 18:35:25 +0300

    example 2 added

---
+++ 2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое целое число: "); 
+int firstNumber = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите второе целое число: ");
+int secondNumber = int.Parse(Console.ReadLine() ?? "");
+
+if(firstNumber > secondNumber) 
+{
+    Console.WriteLine($"a = {firstNumber}, b = {secondNumber}, max = {firstNumber}");
+}
+else 
+{
+    Console.WriteLine($"a = {firstNumber}, b = {secondNumber}, max = {secondNumber}");
+}
