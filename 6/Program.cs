319366677ba8076012f75c5de03443d805f92bcb
Andrei Aliaksandrau <aleksandrov.a.95@yandex.ru>
2022-12-16 19:04:36 +0300

    example 6 added

---
+++ 6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Является ли число четным?"); 
+Console.Write("Введите целое число: "); 
+int number = int.Parse(Console.ReadLine() ?? "");
+
+if(number % 2 == 0) 
+{
+    Console.WriteLine($"{number} -> да");
+}
+else
+{
+    Console.WriteLine($"{number} -> нет");
+}
