40ecb83ec4840dcc4fca2b72d668cee394d1c584
Andrei Aliaksandrau <aleksandrov.a.95@yandex.ru>
2022-12-16 18:50:38 +0300

    example 4 added

---
+++ 4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите первое целое число: "); 
+int firstNumber = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите второе целое число: "); 
+int secondNumber = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите третье целое число: "); 
+int thirdNumber = int.Parse(Console.ReadLine() ?? "");
+
+int max = firstNumber;
+
+if(secondNumber > max) 
+{
+    max = secondNumber;
+}
+
+if(thirdNumber > max) 
+{
+    max = thirdNumber;
+} 
+
+Console.WriteLine($"a = {firstNumber}, b = {secondNumber}, c = {thirdNumber} -> max = {max}");
